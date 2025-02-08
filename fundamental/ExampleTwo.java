bd6fbb409dbfb56fe934ab5029e8777ad2d51be0
John Paul Gabule <lucas.gabule@gmail.com>
2025-02-08 15:29:38 +0800

    java: intro

---
+++ fundamental/ExampleTwo.java
@@ -0,0 +1,15 @@
+package fundamental;
+
+public class ExampleTwo {
+	public static void main(String[] args) {
+		int var1;
+		float var2;
+
+		var1 = 1025;
+		System.out.println("Var1 contains" + var1);
+		
+		var2 = var1 / 2;
+		System.out.println("Var2 contains var1 / 2: ");
+		System.out.println(var2);
+	}
+}
